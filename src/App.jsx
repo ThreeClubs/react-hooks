b95840b28d46486b417ac0de231f97cb0d1c7278
Andrew Grenon <andrew.grenon@gmail.com>
2023-01-16 00:08:10 +1300

    init com

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(() => {
+    return 0;
+  });
+
+  function decrementCount() {
+    setCount((prevCount) => prevCount - 1);
+  }
+
+  function incrementCount() {
+    setCount((prevCount) => prevCount + 1);
+  }
+
+  return (
+    <div className="font">
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <button onClick={incrementCount}>+</button>
+    </div>
+  );
+}
+
+export default App;
